c6966bd9dec4b09780d6596010412f9e6e4a725a
pedro costa <pedro.santos405@outlook.com>
2022-06-06 16:26:28 -0300

    Correção Requisitos About e App

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,26 +4,17 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('se há um conjunto de links de navegação', () => {
+test('if About Page was render correctly', () => {
   const { history } = renderWithRouter(<App />);
   const imgAboutSrc = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
-
-  const homeLink = screen.getByRole('link', { name: /Home/i });
-  userEvent.click(homeLink);
-  expect(history.location.pathname).toBe('/');
-  expect(homeLink).toBeInTheDocument();
-
   const aboutLink = screen.getByRole('link', { name: /About/i });
+
   expect(aboutLink).toBeInTheDocument();
   userEvent.click(aboutLink);
   expect(history.location.pathname).toBe('/about');
+
   const titleAboutPokedex = screen.getByRole('heading', { name: /About Pokédex/i });
   const imgAbout = screen.getByRole('img', { name: /Pokédex/i });
-  expect(imgAbout.src).toContain(imgAboutSrc);
   expect(titleAboutPokedex).toBeInTheDocument();
-
-  const favoritesLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
-  expect(favoritesLink).toBeInTheDocument();
-  userEvent.click(favoritesLink);
-  expect(history.location.pathname).toBe('/favorites');
+  expect(imgAbout.src).toContain(imgAboutSrc);
 });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('if has nav link', () => {
+  const { history } = renderWithRouter(<App />);
+  const homeLink = screen.getByRole('link', { name: /Home/i });
+  userEvent.click(homeLink);
+  expect(history.location.pathname).toBe('/');
+
+  const aboutLink = screen.getByRole('link', { name: /About/i });
+  expect(aboutLink).toBeInTheDocument();
+  userEvent.click(aboutLink);
+  expect(history.location.pathname).toBe('/about');
+
+  const favoritesLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+  expect(favoritesLink).toBeInTheDocument();
+  userEvent.click(favoritesLink);
+  expect(history.location.pathname).toBe('/favorites');
+
+  history.push('/qualquercoisa');
+  const headingPageNotFound = screen.getByRole('heading',
+    { name: /Page requested not found/i });
+  expect(headingPageNotFound).toBeInTheDocument();
+});
