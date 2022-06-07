0ab3109a7bf2a68f3df490d0ec460452128c5379
pedro costa <pedro.santos405@outlook.com>
2022-06-07 15:20:29 -0300

    Requisito Component Pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+test('if Pokemon component was render correctly', () => {
+  renderWithRouter(
+    <Pokemon
+      isFavorite
+      pokemon={ pokemons[4] }
+      showDetailsLink
+    />,
+  );
+  const ALAKAZAM_IMAGE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/65.png';
+
+  const pokemonName = screen.getByTestId('pokemon-name');
+  expect(pokemonName).toHaveTextContent('Alakazam');
+
+  const pokemonType = screen.getByTestId('pokemon-type');
+  expect(pokemonType).toHaveTextContent('Psychic');
+
+  const pokemonImage = screen.getByRole('img', { name: /Alakazam sprite/i });
+  expect(pokemonImage).toHaveAttribute('src', ALAKAZAM_IMAGE_URL);
+
+  const pokemonDetailsLink = screen.getByRole('link', { name: /more details/i });
+  expect(pokemonDetailsLink).toHaveAttribute('href', `/pokemons/${pokemons[4].id}`);
+
+  const pokemonFavoriteIcon = screen.getByRole('img',
+    { name: /Alakazam is marked as favorite/ });
+  expect(pokemonFavoriteIcon).toBeInTheDocument();
+  expect(pokemonFavoriteIcon).toHaveAttribute('src', '/star-icon.svg');
+});
